09922d16270a9877da0b339e208d63b274ddf456
mehrez <contact.mehrezlabidi@gmail.com>
2020-11-14 19:35:31 +0100

    script add alias git

---
+++ aliasgit.sh
@@ -0,0 +1,19 @@
+
+if [[ -z "$@" ]]; then
+    echo >&2 "You must supply an argument!"
+    exit 1
+elif [[ ! -d "$@" ]]; then
+    echo >&2 "$@ is not a valid directory!"
+    exit 1
+fi
+
+APPLICATION_PATH=$@
+cd "${APPLICATION_PATH}"
+pwd
+echo "[alias]" >> .git/config
+echo "        co = checkout" >> .git/config
+echo "        br = branch" >> .git/config
+echo "        ci = commit" >> .git/config
+echo "        st = status" >> .git/config
+echo "        log = log --pretty=oneline --abbrev-commit --graph --decorate" >> .git/config
+
